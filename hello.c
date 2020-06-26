7acf4edc5875c843321567ee0300a268e94583ec
whodoor <whodoor@users.noreply.github.com>
2020-06-26 21:40:25 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
+    //asks user for name
     string yourName = get_string("What's your name?");
-    
+    //prints "hello," followed by user's name
     printf("hello, %s\n", yourName);
 }
