16c88d7ec829e746eaaf42e7e14412d0ccccc477
whodoor <whodoor@users.noreply.github.com>
2020-07-02 08:20:43 +0000

    automated commit by check50 [check50=True]

---
+++ caesar.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, string argv[])
+{
+    string cipher;
+
+    if(argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    cipher = argv[1];
+    bool intOrNot = true;
+    int len = strlen(cipher);
+    for(int i=0; i<len; i++)
+    {
+        if(cipher[i] <= 48 || cipher[i] >= 57)
+        {
+            intOrNot = false;
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+
+    int rotate = atoi(argv[1]);
+
+    string plaintext = get_string("plaintext:\n");
+    int len2 = strlen(plaintext);
+    for(int i=0; i<len2; i++)
+    {
+        plaintext[i] += rotate%26;
+
+        if((plaintext[i] > 90 && plaintext[i] < 97) || plaintext[i] > 122)
+        {
+            plaintext[i] = plaintext[i] - 26;
+        }
+    }
+
+    printf("ciphertext: %s\n", plaintext);
+}
