5aff9524ff832cdc48a694bcab4517537516cc24
whodoor <whodoor@users.noreply.github.com>
2020-07-02 05:02:02 +0000

    automated commit by check50 [check50=True]

---
+++ readability.c
@@ -0,0 +1,57 @@
+#include <cs50.h>
+#include <string.h>
+#include <stdio.h>
+
+double grade(string sample);
+
+int main(void)
+{
+    string text = get_string("Text: ");
+    double mygrade = grade(text);
+
+    int index = mygrade;
+
+    if(-mygrade+index+1 < mygrade-index)
+    {
+        printf("Grade %i\n", index+1);
+    }
+    else
+    {
+        printf("Grade %i\n", index);
+    }
+
+}
+
+
+double grade(string sample)
+{
+    int i=0;
+    double letters=0, words=0, sentences=0;
+    int n = strlen(sample);
+    while(i<n)
+    {
+        // letter counter: is this character a-z
+        if((sample[i]>=65 && sample[i]<=90) || (sample[i]>=97 && sample[i]<=122))
+        {
+            letters++;
+        }
+        // sentence counter: is there end-sentence punctuation?
+        if(sample[i]=='.' || sample[i]=='?' || sample[i]=='!')
+        {
+            sentences++;
+        }
+        // word counter: is this character sin a-z? If it is, then is the next character non A-Z or the end of the text?
+        if( ((sample[i]>=65 && sample[i]<=90) || (sample[i]>=97 && sample[i]<=122)) && (sample[i+1] == '\0' || (!(sample[i+1]>=65 && sample[i+1]<=90) && !(sample[i+1]>=97 && sample[i+1]<=122)) ) )
+        {
+            words++;
+        }
+        i++;
+    }
+
+    double L = letters/words * 100;
+    double S = sentences/words * 100;
+
+    double thisGrade = 0.0588 * L - 0.296 * S - 15.8;
+
+    return thisGrade;
+}
