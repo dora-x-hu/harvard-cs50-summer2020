78d7ab45883ccc7d626be3df36227bf8a80d49cb
whodoor <whodoor@users.noreply.github.com>
2020-07-07 18:55:14 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -58,7 +58,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 
     for(int i = 0; i < height; i++)
     {
-        for(int j = 0; j < width; j++)
+        for(int j = 0; j < width/2; j++)
         {
             leftRed = image[i][j].rgbtRed;
             leftGreen = image[i][j].rgbtGreen;
