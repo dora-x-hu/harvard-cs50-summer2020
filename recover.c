4ae4fe51f2e341bf38704444842f33a0cb68e205
whodoor <whodoor@users.noreply.github.com>
2020-07-09 15:32:26 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 
         if(start)
             {
-                fwrite(filename, 1, 512, img);
+                fwrite(buffer, 1, 512, img);
             }
 
     }
