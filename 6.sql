65a446f4d0d92db2ff01934ce83e8d018912f9df
whodoor <whodoor@users.noreply.github.com>
2020-07-17 22:02:58 +0000

    automated commit by check50 [check50=True]

--- 6.sql
+++ 6.sql
@@ -1,4 +1,4 @@
 /* single column, single row
 includes: average rating of all movies released in 2012 */
-SELECT (SUM(rating)/COUNT(rating)) FROM ratings
+SELECT (SUM(rating*votes)/SUM(votes)) FROM ratings
 WHERE movie_id = (SELECT id FROM movies WHERE year = 2012);
