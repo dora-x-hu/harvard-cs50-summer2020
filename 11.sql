e104f7bb2745ace6c67c11b5b0f285e4a8eb4edf
whodoor <whodoor@users.noreply.github.com>
2020-07-18 05:11:52 +0000

    automated commit by check50 [check50=True]

--- 11.sql
+++ 11.sql
@@ -3,14 +3,14 @@ include: movies starring Chadwick Boseman, top 5 ratings
 order: descending
 */
 
-SELECT title
+SELECT title     /* movie titles are what we output */
 FROM movies
-JOIN stars
-ON movies.id = stars.movie_id
+JOIN stars       /* but we also need to know which people are stars */
+ON movies.id = stars.movie_id  /* this movie is in the stars table */
 JOIN people
-ON stars.person_id = people.id
+ON stars.person_id = people.id /* and the person we're looking for is a star in said movie */
 AND people.name = "Chadwick Boseman"
-JOIN ratings
+JOIN ratings                   /* we need to know ratings to rank them */
 ON movies.id = ratings.movie_id
 ORDER BY ratings.rating DESC
 LIMIT 5;
